452a83d5faa17c3c7477771bc817e0d2787a9301
Henning Jacobs <henning@zalando.de>
2019-03-13 20:40:00 +0100

    fix test to not load user kubeconfig

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,17 +1,35 @@
 import io
+import pytest
 
 from pykube.console import main
 
 
-def test_main_hello_world(monkeypatch, capsys):
-    monkeypatch.setattr('sys.stdin', io.StringIO('print("Hello World!")'))
-    main([])
+@pytest.fixture
+def kubeconfig(tmpdir):
+    kubeconfig = tmpdir.join('kubeconfig')
+    kubeconfig.write('''
+apiVersion: v1
+clusters:
+- cluster: {server: 'https://localhost:9443'}
+  name: test
+contexts:
+- context: {cluster: test}
+  name: test-context
+current-context: test-context
+kind: Config
+    ''')
+    return kubeconfig
+
+
+def test_main_current_context(monkeypatch, capsys, kubeconfig):
+    monkeypatch.setattr('sys.stdin', io.StringIO('print(f"current context: {config.current_context}")'))
+    main(['--kubeconfig', str(kubeconfig)])
     captured = capsys.readouterr()
-    assert 'Hello World!' in captured.out
+    assert 'current context: test-context' in captured.out
 
 
-def test_main_script(monkeypatch, capsys):
+def test_main_script(monkeypatch, capsys, kubeconfig):
     monkeypatch.setattr('pykube.HTTPClient.version', ('1', '13'))
-    main(['-c', 'api.version'])
+    main(['--kubeconfig', str(kubeconfig), '-c', 'api.version'])
     captured = capsys.readouterr()
     assert "('1', '13')\n" == captured.out
