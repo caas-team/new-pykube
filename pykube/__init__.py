88cdc6edf5fff546303ab5d8d83f9fe553c7f005
Henning Jacobs <henning@zalando.de>
2019-07-12 08:28:25 +0200

    v0.27

--- pykube/__init__.py
+++ pykube/__init__.py
@@ -2,7 +2,7 @@
 Python client for Kubernetes
 """
 
-__version__ = '0.26'
+__version__ = '0.27'
 
 from .config import KubeConfig  # noqa
 from .exceptions import KubernetesError, PyKubeError, ObjectDoesNotExist  # noqa
