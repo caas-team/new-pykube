6e84aad010982993f7a102e19260ebf97c45d57b
Henning Jacobs <henning@zalando.de>
2020-07-02 21:54:38 +0200

    v20.7.1

--- pykube/__init__.py
+++ pykube/__init__.py
@@ -2,7 +2,7 @@
 Python client for Kubernetes
 """
 
-__version__ = "20.7.0"
+__version__ = "20.7.1"
 
 from .config import KubeConfig  # noqa: F401
 from .exceptions import KubernetesError, PyKubeError, ObjectDoesNotExist  # noqa: F401
