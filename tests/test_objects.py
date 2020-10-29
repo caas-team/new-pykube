94975ed41b751c6e2e3a51537457e1b10e829ca5
Łukasz Piątkowski <piontec@gmail.com>
2020-10-29 15:17:25 +0100

    improve test coverage: provide tests for Service.proxy_http_request

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,8 @@
+from typing import Dict, Any, Optional
 from unittest.mock import MagicMock
 
 import pytest
+from pytest_mock import MockFixture
 
 import pykube
 from pykube.objects import NamespacedAPIObject
@@ -67,3 +69,43 @@ def test_update():
     pod.update(is_strategic=False)
     assert pod.metadata == {"name": "john"}
     assert pod.annotations == {}
+
+
+@pytest.mark.parametrize(
+    "port,expected_request_kwargs",
+    [
+        # empty port - default service port should be used
+        (
+                None,
+                {"url": "services/test_service:9090/proxy//", "namespace": "test_namespace", "version": "v1"}
+        ),
+        # override the default port, should be included in url
+        (
+                8080,
+                {"url": "services/test_service:8080/proxy//", "namespace": "test_namespace", "version": "v1"},
+        ),
+    ],
+)
+def test_service_proxy_http_request(mocker: MockFixture, port: Optional[int],
+                                    expected_request_kwargs: Dict[str, Any]) -> None:
+    mock_client = mocker.MagicMock(spec=pykube.http.HTTPClient)
+
+    service = pykube.Service(mock_client, {
+        "apiVersion": "core/v1",
+        "kind": "Service",
+        "metadata": {
+            "name": "test_service",
+            "namespace": "test_namespace",
+        },
+        "spec": {
+            "ports": [
+                {"port": 9090}
+            ]
+        }
+    })
+    service.proxy_http_request("GET", "/", port)
+
+    request = mock_client.request
+    assert request.called
+    assert request.call_args_list[0][0] == ("GET",)
+    assert request.call_args_list[0][1] == expected_request_kwargs
